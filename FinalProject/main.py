0fdb3c32d5b9de6cf32fd16c4e2f60ac8ea1106e
abrownie91 <116902819+abrownie91@users.noreply.github.com>
2024-12-03 14:13:16 -0500

    Update main.py

--- FinalProject/main.py
+++ FinalProject/main.py
@@ -3,10 +3,25 @@ from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.textinput import TextInput
 from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import StringProperty, ObjectProperty
 
 
 class MainScreen(Screen):
-    pass
+    password_input = ObjectProperty(None)
+    confirm_input = ObjectProperty(None)
+    feedback_label = ObjectProperty(None)
+
+    def check_password_match(self):
+
+        password = self.password_input.text
+        confirm_password = self.confirm_input.text
+
+        if password == confirm_password and password:
+            self.feedback_label.text = "Password Matches"
+            self.feedback_label.color = (0, 1, 0, 1)
+        else:
+            self.feedback_label.text = "Password does not match"
+            self.feedback_label.color = (1, 0, 0, 1)
 
 class SecondScreen(Screen):
     pass
